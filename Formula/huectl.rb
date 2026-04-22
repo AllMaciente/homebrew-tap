289ca54753c0058562e383888ac8350a06091afd
Allan <135379484+AllMaciente@users.noreply.github.com>
2026-04-22 17:50:37 -0400

    Fix URL format for huectl version 0.1.1

--- Formula/huectl.rb
+++ Formula/huectl.rb
@@ -1,7 +1,7 @@
 class Huectl < Formula
   desc "Ferramenta de CLI"
   homepage "https://github.com/AllMaciente/Huectl"
-  url "https://github.com/AllMaciente/Huectl/archive/refs/tags/0.1.1.tar.gz"
+  url "https://github.com/AllMaciente/Huectl/archive/refs/tags/v0.1.1.tar.gz"
   sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed"
   license "MIT" 
   depends_on "rust" => :build
