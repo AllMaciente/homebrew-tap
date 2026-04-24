4756be116491d3e99f4e9dcae5e160232342f781
Allan <135379484+AllMaciente@users.noreply.github.com>
2026-04-24 17:43:28 -0400

    Update URL and SHA256 for version 0.1.2

--- Formula/huectl.rb
+++ Formula/huectl.rb
@@ -1,8 +1,8 @@
 class Huectl < Formula
   desc "Ferramenta de CLI"
   homepage "https://github.com/AllMaciente/Huectl"
-  url "https://github.com/AllMaciente/Huectl/archive/refs/tags/v0.1.1.tar.gz"
-  sha256 "f3b2455696aa532b9a70900ad7791272b6db1874ff4a24743687e15e9ec186ff"
+  url "https://github.com/AllMaciente/Huectl/archive/refs/tags/0.1.2.tar.gz"
+  sha256 "a018fd0d2c8cce0890b605ef9d39c74fe2201688835cac567a9ec41d69a3b866"
   license "MIT" 
   depends_on "rust" => :build
 
