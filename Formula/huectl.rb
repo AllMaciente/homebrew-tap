72638f9f33d3986743cbf47568c3a70524b7b3dc
Allan <135379484+AllMaciente@users.noreply.github.com>
2026-09-15 15:31:12 -0300

    Update SHA256 checksum for huectl.rb

--- Formula/huectl.rb
+++ Formula/huectl.rb
@@ -2,7 +2,7 @@ class Huectl < Formula
   desc "Ferramenta de CLI"
   homepage "https://github.com/AllMaciente/Huectl"
   url "https://github.com/AllMaciente/Huectl/archive/refs/tags/0.2.0.tar.gz"
-  sha256 "2f36af81b6be1f84f1004a1df100122a3500276e66cdc8a16e82b43ba98a62b7"
+  sha256 "09345d9dcdc1ce742274ac30df8dc2499234861ff8a7c3428cc414073db267d6"
   license "MIT" 
   depends_on "rust" => :build
 
